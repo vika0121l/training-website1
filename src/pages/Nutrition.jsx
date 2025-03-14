2d912b5fb7251c13631b11d5c94d38646db2b17f
vika0121l <pemma0121@gmail.com>
2025-03-14 12:33:15 +0000

    Харчування

--- src/pages/Nutrition.jsx
+++ src/pages/Nutrition.jsx
@@ -2,12 +2,12 @@ function Nutrition() {
   return (
     <main className="container px-4 py-4 flex-grow-1">
       <section>
-        <h2 className="h2 text-success mb-4">Харчування зайців</h2>
-        <p>Зайці є травоїдними тваринами. Їх раціон включає:</p>
+        <h2 className="h2 text-success mb-4">Харчування Снігових барсів</h2>
+        <p>Сніговий барс є хижаком, що живе і полює поодинці . Їх раціон включає:</p>
         <ul className="list-group">
-          <li className="list-group-item">Траву, листя і кору дерев.</li>
-          <li className="list-group-item">Овочі, такі як морква та капуста.</li>
-          <li className="list-group-item">Воду, яку вони споживають у великих кількостях.</li>
+          <li className="list-group-item">Основною здобиччю є нахур блакитний.</li>
+          <li className="list-group-item">Козел гвинторогий, архар, баран азійський, тар, козоріг суматранський, горал, кабарга альпійська, свиня дика, пантолопа тибетська, ґоа, джейран, кулан, як дикий, бабак, заєць, пискуха, полівка.</li>
+          <li className="list-group-item">У голодні часи він не гребує мишами та птахами.</li>
         </ul>
       </section>
     </main>
