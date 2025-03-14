f7f2c8aeec52b4ec00f91b452068da2376ccdf59
vika0121l <pemma0121@gmail.com>
2025-03-14 12:53:47 +0000

    Популяція

--- src/pages/Population.jsx
+++ src/pages/Population.jsx
@@ -12,7 +12,7 @@ function Population() {
               <a href="#continentsList" className="nav-link" data-bs-toggle="collapse">Континенти</a>
               <a href="#subcontinentsList" className="nav-link" data-bs-toggle="collapse">Субконтиненти</a>
               <a href="#countriesList" className="nav-link" data-bs-toggle="collapse">Країни</a>
-              <a href="#introducedList" className="nav-link" data-bs-toggle="collapse">Інтродуковані в</a>
+              <a href="#regionsList" className="nav-link" data-bs-toggle="collapse">Регіони</a>
               <a href="#biogeographicList" className="nav-link" data-bs-toggle="collapse">Біогеографічні зони</a>
               <a href="#biomesList" className="nav-link" data-bs-toggle="collapse">WWF Біоми</a>
             </ul>
@@ -20,7 +20,7 @@ function Population() {
         </aside>
 
         <article className="col-md-9">
-          <h2 className="h2 text-success mb-4">Ареал поширення зайців</h2>
+          <h2 className="h2 text-success mb-4">Ареал поширення Снігових барсів</h2>
           
           <section id="continents" className="mt-4">
             <h3>
@@ -35,9 +35,7 @@ function Population() {
             </h3>
             <div className="collapse" id="continentsList">
               <ul className="list-group mb-3">
-                <li className="list-group-item">Європа</li>
                 <li className="list-group-item">Азія</li>
-                <li className="list-group-item">Північна Америка</li>
               </ul>
             </div>
           </section>
@@ -55,7 +53,7 @@ function Population() {
             </h3>
             <div className="collapse" id="subcontinentsList">
               <ul className="list-group mb-3">
-                <li className="list-group-item">Західна Азія</li>
+                <li className="list-group-item">Південна Азія</li>
                 <li className="list-group-item">Центральна Азія</li>
               </ul>
             </div>
@@ -74,72 +72,35 @@ function Population() {
             </h3>
             <div className="collapse" id="countriesList">
               <ul className="list-group mb-3 list-columns">
-                <li className="list-group-item">Албанія</li>
-                <li className="list-group-item">Австрія</li>
-                <li className="list-group-item">Білорусь</li>
-                <li className="list-group-item">Бельгія</li>
-                <li className="list-group-item">Боснія і Герцеговина</li>
-                <li className="list-group-item">Болгарія</li>
-                <li className="list-group-item">Хорватія</li>
-                <li className="list-group-item">Чехія</li>
-                <li className="list-group-item">Данія</li>
-                <li className="list-group-item">Естонія</li>
-                <li className="list-group-item">Фінляндія</li>
-                <li className="list-group-item">Франція</li>
-                <li className="list-group-item">Німеччина</li>
-                <li className="list-group-item">Греція</li>
-                <li className="list-group-item">Угорщина</li>
-                <li className="list-group-item">Іран</li>
-                <li className="list-group-item">Ірак</li>
-                <li className="list-group-item">Ізраїль</li>
-                <li className="list-group-item">Італія</li>
-                <li className="list-group-item">Латвія</li>
-                <li className="list-group-item">Ліхтенштейн</li>
-                <li className="list-group-item">Литва</li>
-                <li className="list-group-item">Люксембург</li>
-                <li className="list-group-item">Північна Македонія</li>
-                <li className="list-group-item">Молдова</li>
-                <li className="list-group-item">Чорногорія</li>
-                <li className="list-group-item">Нідерланди</li>
-                <li className="list-group-item">Польща</li>
-                <li className="list-group-item">Румунія</li>
-                <li className="list-group-item">Росія</li>
-                <li className="list-group-item">Сербія</li>
-                <li className="list-group-item">Словаччина</li>
-                <li className="list-group-item">Словенія</li>
-                <li className="list-group-item">Іспанія</li>
-                <li className="list-group-item">Швейцарія</li>
-                <li className="list-group-item">Сирія</li>
-                <li className="list-group-item">Туреччина</li>
-                <li className="list-group-item">Україна</li>
+                <li className="list-group-item">Бутан</li>
+                <li className="list-group-item">Китайська Народна Республіка</li>
+                <li className="list-group-item">Індія</li>
+                <li className="list-group-item">Казахстан</li>
+                <li className="list-group-item">Киргизстан</li>
+                <li className="list-group-item">Монголія</li>
+                <li className="list-group-item">Непал</li>
+                <li className="list-group-item">Пакистан</li>
+                <li className="list-group-item">Узбекистан</li>
+                <li className="list-group-item">Афганістан</li>
               </ul>
             </div>
           </section>
 
-          <section id="introduced" className="mt-4">
+          <section id="regions" className="mt-4">
             <h3>
               <button className="btn btn-success w-100 text-start" 
                       type="button" 
                       data-bs-toggle="collapse" 
-                      data-bs-target="#introducedList" 
+                      data-bs-target="#regionsList" 
                       aria-expanded="false" 
-                      aria-controls="introducedList">
-                Інтродуковані види
+                      aria-controls="regionsList">
+                Регіони
               </button>
             </h3>
-            <div className="collapse" id="introducedList">
+            <div className="collapse" id="regionsList">
               <ul className="list-group mb-3">
-                <li className="list-group-item">Канада</li>
-                <li className="list-group-item">Сполучені Штати Америки</li>
-                <li className="list-group-item">Бразилія</li>
-                <li className="list-group-item">Чилі</li>
-                <li className="list-group-item">Аргентина</li>
-                <li className="list-group-item">Уругвай</li>
-                <li className="list-group-item">Парагвай</li>
-                <li className="list-group-item">Болівія</li>
-                <li className="list-group-item">Перу</li>
-                <li className="list-group-item">Австралія</li>
-                <li className="list-group-item">Нова Зеландія</li>
+                <li className="list-group-item">Тибетський автономний район</li>
+                <li className="list-group-item">Юньнань</li>
               </ul>
             </div>
           </section>
@@ -157,10 +118,8 @@ function Population() {
             </h3>
             <div className="collapse" id="biogeographicList">
               <ul className="list-group mb-3">
-                <li className="list-group-item">Нотогея</li>
-                <li className="list-group-item">Неарктика</li>
-                <li className="list-group-item">Неотропіка</li>
                 <li className="list-group-item">Палеарктика</li>
+                <li className="list-group-item">Індомалайя</li>
               </ul>
             </div>
           </section>
@@ -178,9 +137,9 @@ function Population() {
             </h3>
             <div className="collapse" id="biomesList">
               <ul className="list-group">
-                <li className="list-group-item">Помірні луки</li>
-                <li className="list-group-item">Савани і чагарники</li>
-                <li className="list-group-item">Субтропічний ліс</li>
+                <li className="list-group-item">Помірні широколистяні та мішані ліси</li>
+                <li className="list-group-item">Гірські луки та чагарники</li>
+                <li className="list-group-item">Хвойні ліси помірної зони</li>
               </ul>
             </div>
           </section>
